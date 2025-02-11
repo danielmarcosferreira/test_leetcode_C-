cb4d8bbba9b4bdf49f8b150d79f9e0efef75c8f8
danielmarcos <daniel.marcos.1038@gmail.com>
2025-02-11 19:56:18 -0300

    feat: finalize and clean up LeetCode solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+
+        // Test Cases
+        Console.WriteLine(solution.DetectCapitalUse("USA"));
+        Console.WriteLine(solution.DetectCapitalUse("FlaG"));
+    }
+}
+
+// To run on the LeetCode site only copy the code below
+public class Solution
+{
+    public bool DetectCapitalUse(string word)
+    {
+        int capitalCount = 0;
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (char.IsUpper(word[i]))
+            {
+                capitalCount++;
+            }
+        }
+
+        return capitalCount == word.Length ||
+            capitalCount == 0 ||
+            (capitalCount == 1 && char.IsUpper(word[0]));
+    }
+}
